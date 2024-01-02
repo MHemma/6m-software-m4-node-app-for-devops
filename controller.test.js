8b2e87b430a9ae187c94b9744db7ab8f5404b19a
Hemma <hemma1108@gmail.com>
2024-01-02 20:46:56 +0800

    Passed test and added config.yml file

--- controller.test.js
+++ controller.test.js
@@ -1,19 +1,28 @@
+// imports the module located at the relative path "./controller" and assigns it to the variable targetFn.
 const targetFn = require("./controller");
+
 
 describe("the print function", ()=>{
 
     // standard best practice
+    // nsures that all mocks created by Jest (such as jest.fn()) are cleared before each test, preventing any potential interference between test cases.
     beforeEach(()=>{
         jest.clearAllMocks();
     })
 
-    it("should respond with 'Hello world!'", ()=>{
+    it("should respond with 'Hello world!", ()=>{
+        // sets up mock request
         const mockReq = {};
+        // sets up mock response with a mocked "send function"
+        // jest.fn() is a Jest utility function that creates a mock function. 
+        // mock send function created using "jest.fn()"
         const mockRes = {
             send:jest.fn()
         }
 
+        // i.e. invoking print function from controller.js
         targetFn(mockReq, mockRes);
-        expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
+        // expected behaviour - always fixed data 
+        expect(mockRes.send).toHaveBeenCalledWith("Hello world!"); // If you change this value, the test will fail.
     })
 })
